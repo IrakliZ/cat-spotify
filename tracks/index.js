84c63dce16936de8031dc3a8603fea6be646a606
Irakli Zhuzhunashvili <irakli.zhuzhunashvili1@gmail.com>
2015-03-02 18:16:57 -0700

    More build files

---
+++ tracks/index.js
@@ -0,0 +1,54 @@
+var tracks = {
+
+    searchByTrack: function(trackName) {
+
+        trackName.replace(" ", "%20")
+
+        $.get("https://api.spotify.com/v1/search?q=" + trackName + "&type=track", function(data) {
+
+            console.log(data)
+            if (data.tracks.items){
+
+                $.get("/cat-spotify/tracks/list.jade", function(template) {
+                    var html = jade.render(template, {
+                        data: data.tracks.items
+                    })
+
+                    $("#list").html(html)
+                })
+
+            }
+
+        })
+
+    },
+
+    findTopTen: function(artistId) {
+        console.log("HIII: " + artistId)
+
+        $.get("https://api.spotify.com/v1/artists/" + artistId + "/top-tracks?country=US", function(data) {
+            console.log(data)
+
+            if (data.tracks){
+
+                $.get("/cat-spotify/tracks/list.jade", function(template) {
+                    var html = jade.render(template, {
+                        data: data.tracks
+                    })
+
+                    $("#list").html(html)
+                })
+
+            }
+
+        })
+    },
+
+    load: function() {
+
+        $.get('/cat-spotify/tracks/ui.jade', function(template) {
+            var html = jade.render(template);
+            $("#searchdiv").html(html);
+        })
+    }
+}
