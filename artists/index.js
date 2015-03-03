949e144264155c2beca9b295714b52fd5e161e5c
Irakli Zhuzhunashvili <irakli.zhuzhunashvili1@gmail.com>
2015-03-02 18:15:38 -0700

    build

--- artists/index.js
+++ artists/index.js
@@ -3,30 +3,29 @@ var artists = {
     searchByName: function(name) {
 
         var name = name || "Tania Bowra"
-
+        
+        name.replace(" ", "%20")
+        
         $.get("https://api.spotify.com/v1/search?q="+ name +"&type=artist", function(data) {
-
+            console.log(data)
             $.get("/cat-spotify/artists/list.jade", function(template) {
                 var html = jade.render(template, {
                     data: data
                 })
                 $("#list").html(html)
             })
-
         })
-
-
     },
 
     load: function() {
 
         $.get("/cat-spotify/artists/ui.jade", function(template) {
             var html = jade.render(template)
-            $("#ui").html(html)
+            $("#searchdiv").html(html)
         })
 
         // default search results
-        bills.searchByName('Tania Bowra')
+        artists.searchByName('Tania Bowra')
 
     }
 
--- js/app.js
+++ js/app.js
@@ -1,12 +1,15 @@
 $( "#artists" ).on( "click", function( event ) {
+    $("#list").empty()
     artists.load()
 })
 
 $( "#albums" ).on( "click", function( event ) {
+    $("#list").empty()
     albums.load()
 })
 
 $("#tracks").on("click", function(event) {
+    $("#list").empty()
     tracks.load()
 })
 
