60cfec18df308d7b82724b604f353d01834a6edb
rshamirza <rshamirza@rgnt2-73-243-dhcp.int.colorado.edu>
2015-03-02 17:21:21 -0700

    artists index.js

---
+++ artists/index.js
@@ -0,0 +1,33 @@
+var artists = {
+
+    searchByName: function(name) {
+
+        var name = name || "Tania Bowra"
+
+        $.get("https://api.spotify.com/v1/search?q="+ name +"&type=artist", function(data) {
+
+            $.get("/cat-spotify/artists/list.jade", function(template) {
+                var html = jade.render(template, {
+                    data: data
+                })
+                $("#list").html(html)
+            })
+
+        })
+
+
+    },
+
+    load: function() {
+
+        $.get("/cat-spotify/artists/ui.jade", function(template) {
+            var html = jade.render(template)
+            $("#ui").html(html)
+        })
+
+        // default search results
+        bills.searchByName('Tania Bowra')
+
+    }
+
+}
---
+++ js/app.js
@@ -0,0 +1,12 @@
+$( "#artists" ).on( "click", function( event ) {
+    artists.load()
+})
+
+$( "#albums" ).on( "click", function( event ) {
+    albums.load()
+})
+
+$("#tracks").on("click", function(event) {
+    tracks.load()
+})
+
