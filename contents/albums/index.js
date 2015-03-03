729ae514d18e2d0c1a8d700499afb045480154c1
Adrian Chen <adrianchen@rgnt2-122-244-dhcp.int.colorado.edu>
2015-03-02 17:05:29 -0700

    file templates

--- contents/albums/index.js
+++ contents/albums/index.js
@@ -1,20 +1,15 @@
 var album = {
 
-    searchByAlbum: function(zipcode) {
+    searchByAlbum: function(title) {
 
-        // search legistalors by zipcode (default to Boulder, 80301)
-        // ref: https://sunlightlabs.github.io/congress/legislators.html
-
-        var zipcode = zipcode || '80301'
-
-        $.get("https://congress.api.sunlightfoundation.com/legislators/locate?zip=" + zipcode, apikey, function(data) {
+        $.get("https://api.spotify.com/v1/search?q="+title+"&type=album" + zipcode, function(data) {
 
             console.log('got ' + data)
-            if (data.results){
+            if (data.items){
 
-                $.get("/sunlight/legislators/list.jade", function(template) {
+                $.get("/cat-spotify/albums/list.jade", function(template) {
                     var html = jade.render(template, {
-                        data: data
+                        data: data.items
                     })
                     console.log(html)
                     $("#list").html(html)
